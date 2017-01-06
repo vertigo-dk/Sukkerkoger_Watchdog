70e1fc04cdc1a5c7b64ec553ea12a2c7d0aaa529
Frederik Tollund Juutilainen <frederikjuutilainen@mbp-1047.home>
2017-01-06 13:08:42 +0100

    checks processes

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 __author__ = 'frederikjuutilainen'
 
-import argparse, threading, time
+import argparse, threading, time, subprocess
 
 #receiving osc
 from pythonosc import dispatcher
@@ -12,26 +12,43 @@ from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
 # Main loop / OSC Listener
-port = 7286
+pi_id = 1
+port_in = 7010 + pi_id
+port_out = 7001
 ip = "127.0.0.1"
 
-def quit(unused_addr):
-    print('quit')
-
 def shutdown(unused_addr):
-    print('shutdown')
+    import os
+    os.system("sudo shutdown -h now")
 
 def give_status():
+# Check processes running and send /ok if video player is running
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="127.0.0.1",
+    help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=port_out,
+    help="The port the OSC server is listening on")
+    args = parser.parse_args()
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
+
+    # check processes running
     while True:
-        print ("Still running")
-        time.sleep(1.0)
+        try:
+            # process running
+            x = subprocess.check_output(['pidof omxplayer.bin -s'])
+            client.send_message("/dead/" + str(pi_id), 0)
+        except FileNotFoundError:
+            # process not running
+            client.send_message("/dead/" + str(pi_id), 1)
+
+        time.sleep(5.0)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip",
                         default=ip, help="The ip to listen on")
     parser.add_argument("--port",
-                        type=int, default=port, help="The port to listen on")
+                        type=int, default=port_in, help="The port to listen on")
     args = parser.parse_args()
 
     dispatcher = dispatcher.Dispatcher()
