7931ea749979be0d1516742a0bd8dfdb256acfca
Frederik Tollund Juutilainen <frederikjuutilainen@mbp-1047.home>
2017-01-06 16:29:06 +0100

    checking running processes

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 __author__ = 'frederikjuutilainen'
 
-import argparse, threading, time, subprocess
+import argparse, threading, time, subprocess, psutil
 
 #receiving osc
 from pythonosc import dispatcher
@@ -36,22 +36,23 @@ def give_status():
 
     # check processes running
     while True:
-        try:
-            # process running
-            x = subprocess.check_output(['pidof omxplayer.bin -s'])
-            client.send_message("/dead/" + str(pi_id), 0)
-            os.system('echo omxplayer running - pid: ' + str(x))
-        except FileNotFoundError:
-            # process not running
-            client.send_message("/dead/" + str(pi_id), 1)
-            os.system('echo omxplayer not running')
+        process_name = "supervisord"
+        process_running = False
+
+        for proc in psutil.process_iter():
+            if proc.name() == process_name:
+                process_running = True
+                print('video_player running')
+
+        if(process_running == False):
+            print('video_player not running')
 
         time.sleep(5.0)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip",
-                        default="192.168.1.62", help="The ip to listen on")
+                        default="127.0.0.1", help="The ip to listen on")
     parser.add_argument("--port",
                         type=int, default=port_in, help="The port to listen on")
     args = parser.parse_args()
